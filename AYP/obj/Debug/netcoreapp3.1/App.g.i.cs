c6ba862fdfcb5bded5fc6b1606f0574cef1c7706
externaluser3 <info@klc.com>
2021-03-07 10:15:34 +0300

    toolbar added.

--- AYP/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ AYP/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -48,7 +48,7 @@ namespace AYP {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -74,7 +74,7 @@ namespace AYP {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
         public static void Main() {
             AYP.App app = new AYP.App();
             app.InitializeComponent();
