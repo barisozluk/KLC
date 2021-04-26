1e520b925d0f55dbcea0238a780f184a3406575c
DESKTOP-86JLPO4\KLC <barisozluk2@gmail.com>
2021-04-26 09:39:52 +0300

    V1.0.4

--- AYP/SettingsPopupWindow.xaml.cs
+++ AYP/SettingsPopupWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace AYP
         public SettingsPopupWindow()
         {
             InitializeComponent();
-            this.Versiyon.Content = "Versiyon : 1.0.3";
+            this.Versiyon.Content = "Versiyon : 1.0.4";
         }
 
         private void VersiyonPopupClose_Click(object sender, RoutedEventArgs e)
